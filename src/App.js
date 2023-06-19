35d59a244aec8b3b323bd3e2d2fd2fc9a7303a96
Monika <monikapati.171995@gmail.com>
2023-06-20 01:25:13 +0530

    updating the logo and link in react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './Vector.svg';
+//import logo from 'https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          //href="https://reactjs.org"
+	  href="https://www.propelleraero.com/dirtmate/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Propeller
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
